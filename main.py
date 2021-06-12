ae0999a942bc082eacb0c24c8c3f4c7d73daac0d
sharon <sharonvazana@gmail.com>
2021-06-13 00:13:46 +0300

    fixes to Simpson

--- main.py
+++ main.py
@@ -1,74 +1,100 @@
 # team members: Sharon Vazana, Hadar Amsalem
 
 # simpson methods
-# שרונית סימפסון עובד, רומברג לא.. יש רק התחלה שלו
+
 
 import math
+import sympy as sp
+from sympy.utilities.lambdify import lambdify
 
 
-def simpson(f ,startPoint, endPoint ,parts):
+def simpson(f, startPoint, endPoint, parts):
+    """
+    :param f: original function
+    :param startPoint: start of range
+    :param endPoint: end of range
+    :param parts: amount of segments
+    :return: approximate area of the integral
+    """
     if parts % 2 == 1:
-        print("the parts need to be zogi")
+        print("Amount of parts must be even")
         return None
-    gap = abs(endPoint-startPoint)/parts
-    appr = f(startPoint)
+    x = sp.symbols('x')
+    func = lambdify(x, f)
+    gap = abs(endPoint - startPoint) / parts
+    appr = func(startPoint)
     for i in range(1, parts):
         if i % 2 == 0:
-            appr += 2 * f((i * gap)+startPoint)
+            appr += 2 * func((i * gap) + startPoint)
         else:
-            appr += 4 * f((i * gap) + startPoint)
-    appr += f(endPoint)
+            appr += 4 * func((i * gap) + startPoint)
+    appr += func(endPoint)
     appr *= 1 / 3 * gap
     return appr
 
 
-def romberg(f ,startPoint, endPoint, epsilon = 0.0001):
-    apprF = iterationRom(f ,startPoint, endPoint, 1)
-    apprS = iterationRom(f ,startPoint, endPoint, 2)
-    apprT = iterationRom(f ,startPoint, endPoint, 3)
-    R1 = apprS + 1/3 * (apprS-apprF)
-    R2 = apprT + 1/3 * (apprT-apprS)
-    i  =4
-    while R1-R2 > epsilon:
-        apprS = apprT
-        apprT = apprT = iterationRom(f ,startPoint, endPoint, i)
-        i += 1
-        R1 = R2
-        R2 = apprT + 1 / 3 * (apprT - apprS)
-    return R2
-
-
-def iterationRom(f ,startPoint, endPoint, parts):
+def errorCalculation(f, parts, endPoint, startPoint):
+    """
+    :param f: original function
+    :param parts: amount of segments
+    :param endPoint: end of range
+    :param startPoint: start of range
+    :return: value of error
+    """
+    x = sp.symbols('x')
     gap = abs(endPoint - startPoint) / parts
-    appr = f(startPoint)
-    div = 2
-    for i in range(1, parts):
-        appr += 2 * f(startPoint + endPoint / div)
-        div += div
-    appr += f(endPoint)
-    appr *= 1 / div * endPoint
-    return appr
+    func = calcDerived(calcDerived(calcDerived(calcDerived(f))))
+    ksi = findUpperBound(func)
+    if ksi is None:
+        print("Couldn't calculate the error")
+        return
+    fourthDerivative = lambdify(x, func)
+    return 1 / 180 * math.pow(gap, 4) * (endPoint - startPoint) * fourthDerivative(ksi)
 
 
-def errorCalculation(f, calc, parts, endPoint, startPoint):
-    gap = abs(endPoint - startPoint) / parts
-    return 1/180 * math.pow(gap, parts) * (endPoint-startPoint) * f(1)
+def findUpperBound(f):
+    """
+    :return: estimated upper bound
+    """
+    x = sp.symbols('x')
+    fTag = calcDerived(f)
+    s = sp.solve(fTag)
+    func = lambdify(x, f)
+    if len(s) > 0:
+        maximum = func(s[0])
+        for i in range(1, len(s)):
+            if func(s[i]) > maximum:
+                maximum = func(s[i])
+        return maximum
+    return None
+
+
+def calcDerived(f):
+    """
+    :param f: original func
+    :return: the derived without lambdify
+    """
+    # calc the derivative from func -> lambdify
+    x = sp.symbols('x')
+    f_prime = f.diff(x)
+    return f_prime
 
 
 def driver():
     """
     the main function of the program
     """
-    f = math.sin
+    x = sp.symbols('x')
+
+    # define function
+    f = sp.sin(x)
 
     # define range
     startPoint = 0
     endPoint = math.pi
-    calc = simpson(f, startPoint, endPoint, 4)
-    print(calc)
-    print(iterationRom(f ,startPoint, endPoint, 5))
-    print(calc)
-    print(errorCalculation(f, calc, 4, endPoint, startPoint))
+
+    print(simpson(f, startPoint, endPoint, 4))
+    print(errorCalculation(f, 4, endPoint, startPoint))
 
 
 driver()
